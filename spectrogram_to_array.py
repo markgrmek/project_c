11c2d2bee1691b7f435d7a7e32fa26a7089e3052
markgrmek <117090094+markgrmek@users.noreply.github.com>
2024-03-14 23:27:49 +0100

    Create spectrogram_to_array.py

---
+++ spectrogram_to_array.py
@@ -0,0 +1,113 @@
+import math
+
+import numpy as np
+from matplotlib import mlab
+
+
+
+def _nearest_pow_2(x):
+    """
+    Find power of two nearest to x
+
+    >>> _nearest_pow_2(3)
+    2.0
+    >>> _nearest_pow_2(15)
+    16.0
+
+    :type x: float
+    :param x: Number
+    :rtype: int
+    :return: Nearest power of 2 to x
+    """
+    a = math.pow(2, math.ceil(np.log2(x)))
+    b = math.pow(2, math.floor(np.log2(x)))
+    if abs(a - x) < abs(b - x):
+        return a
+    else:
+        return b
+
+
+def spectrogram(data, samp_rate, 
+                per_lap=0.9,
+                wlen=None,
+                log=False,
+                dbscale=False,
+                mult=8.0,
+                clip=[0.0, 1.0]) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+    
+    """Function copied from official ObSpy GitHub and removed the plotting part
+    and made so it returns just the spectrogram array, freq array and time array.
+    Original code available at: https://github.com/obspy/obspy/blob/master/obspy/imaging/spectrogram.py
+    
+    Nice loking spectrograms with reasnoablly quick computation: log = True, wlen = 2, per_lap = 0.5, dbscale = False, mult = 8, clip = [0,1]"""
+    
+    samp_rate = float(samp_rate)
+
+    # set wlen from samp_rate if not specified otherwise
+    if not wlen:
+        wlen = 128 / samp_rate
+
+    npts = len(data)
+
+    # nfft needs to be an integer, otherwise a deprecation will be raised
+    # XXX add condition for too many windows => calculation takes for ever
+    nfft = int(_nearest_pow_2(wlen * samp_rate))
+
+    if npts < nfft:
+        msg = (f'Input signal too short ({npts} samples, window length '
+               f'{wlen} seconds, nfft {nfft} samples, sampling rate '
+               f'{samp_rate} Hz)')
+        raise ValueError(msg)
+
+    if mult is not None:
+        mult = int(_nearest_pow_2(mult))
+        mult = mult * nfft
+    nlap = int(nfft * float(per_lap))
+
+    data = data - data.mean()
+    end = npts / samp_rate
+
+    # Here we call not plt.specgram as this already produces a plot
+    # matplotlib.mlab.specgram should be faster as it computes only the
+    # arrays
+    # XXX mlab.specgram uses fft, would be better and faster use rfft
+    specgram, freq, time = mlab.specgram(data, Fs=samp_rate, NFFT=nfft,
+                                         pad_to=mult, noverlap=nlap)
+
+    if len(time) < 2:
+        msg = (f'Input signal too short ({npts} samples, window length '
+               f'{wlen} seconds, nfft {nfft} samples, {nlap} samples window '
+               f'overlap, sampling rate {samp_rate} Hz)')
+        raise ValueError(msg)
+
+    # db scale and remove zero/offset for amplitude
+    if dbscale:
+        specgram = 10 * np.log10(specgram[1:, :])
+    else:
+        specgram = np.sqrt(specgram[1:, :])
+    freq = freq[1:]
+
+    vmin, vmax = clip
+    if vmin < 0 or vmax > 1 or vmin >= vmax:
+        msg = "Invalid parameters for clip option."
+        raise ValueError(msg)
+    _range = float(specgram.max() - specgram.min())
+    vmin = specgram.min() + vmin * _range
+    vmax = specgram.min() + vmax * _range
+
+    if log:
+        # calculate half bin width
+        halfbin_time = (time[1] - time[0]) / 2.0
+        halfbin_freq = (freq[1] - freq[0]) / 2.0
+        # pcolor expects one bin more at the right end
+        freq = np.concatenate((freq, [freq[-1] + 2 * halfbin_freq]))
+        time = np.concatenate((time, [time[-1] + 2 * halfbin_time]))
+        # center bin
+        time -= halfbin_time
+        freq -= halfbin_freq
+
+    else:
+        # this method is much much faster!
+        specgram = np.flipud(specgram)
+
+    return specgram, freq, time
